7aa70df0dd9d7d3ebbb0ac2a790d2c7a0ab9e068
Victor Hugo <victor.vh340@gmail.com>
2025-06-30 00:10:42 +0000

    Unhandled Runtime Error
    Error: Invalid src prop (https://www.valuehost.

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.valuehost.com.br',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
